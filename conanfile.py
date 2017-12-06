5646e37deb91a7dbe63ad345fc037794652a5815
Rene Rivera <grafikrobot@gmail.com>
2017-12-06 08:13:46 -0600

    Require generator.

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,9 @@ from conans import ConanFile, tools
 class BoostGraph_ParallelConan(ConanFile):
     name = "Boost.Graph_Parallel"
     version = "1.65.1"
-    requires =  "Boost.Level14Group/1.65.1@bincrafters/testing"
+    requires = \
+        "Boost.Generator/1.65.1@bincrafters/testing", \
+        "Boost.Level14Group/1.65.1@bincrafters/testing"
     lib_short_names = ["graph_parallel"]
     is_header_only = False
     is_in_cycle_group = True
@@ -12,10 +14,12 @@ class BoostGraph_ParallelConan(ConanFile):
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-graph_parallel"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
     license = "www.boost.org/users/license.html"
     short_paths = True
     build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    generators = "boost"
+    settings = "os", "arch", "compiler", "build_type"
 
     @property
     def env(self):
