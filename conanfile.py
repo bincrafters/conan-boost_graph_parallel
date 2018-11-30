dddd1b3907ce7be3372a0ffc45c0883b5c4896cd
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:12 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,15 +4,11 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostGraph_ParallelConan(base.BoostBaseConan):
     name = "boost_graph_parallel"
     url = "https://github.com/bincrafters/conan-boost_graph_parallel"
     lib_short_names = ["graph_parallel"]
-    cycle_group = "boost_level14group"
-    b2_requires = [
-        "boost_level14group",
-    ]
-
-
+    cycle_group = "boost_cycle_group_d"
+    b2_requires = ["boost_cycle_group_d"]
